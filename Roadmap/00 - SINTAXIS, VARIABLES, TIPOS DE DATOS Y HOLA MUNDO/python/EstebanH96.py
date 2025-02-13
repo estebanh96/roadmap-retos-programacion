690142aca490adaa61e98a0f3395a7e89aff254b
estebanh96 <hector.higueraqf@hotmail.com>
2025-02-12 23:46:08 -0500

    EstebanH96.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/EstebanH96.py
@@ -0,0 +1,19 @@
+# https://www.python.org
+
+# Comentario en una linea
+
+"""
+Eto es un comentario 
+en varias
+líneas
+"""
+
+'''
+Eto es un comentario 
+en varias
+líneas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
